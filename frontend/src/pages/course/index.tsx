78703ced87be78bbdfcdd0fcd3d58f08503caeeb
Tasmia Zerin <ttaosnmniya.6638.cross@gmail.com>
2022-08-05 01:38:03 +0600

    Fixed navbar issue

---
+++ frontend/src/pages/course/index.tsx
@@ -0,0 +1,35 @@
+import Image from 'next/image';
+import styles from 'styles/Course.module.scss';
+import Navbar from 'components/navbar/navbar';
+import image from '../../../public/quizButton.png'
+
+export default function Course() {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <div className={styles.bgcolor}>
+                <div className={styles.heading}>
+                </div>
+                <div className={styles.ques}>
+                    <h1>Chlorine atom forms -</h1>
+                </div>
+                <div className={styles.answers}>
+                    <div className="grid grid-cols-2 gap-2">
+                        <div className={styles.optionsL}>
+                            <h1 className={styles.singleOptionL}>Covalent bond with Hydrogen</h1>
+                        </div>
+                        <div className={styles.optionsR}>
+                            <h1 className={styles.singleOptionR}>Ionic bond with Calcium</h1>
+                        </div>
+                        <div className={styles.optionsL}>
+                            <h1 className={styles.singleOptionL}>Ionic bond with Carbon</h1>
+                        </div>
+                        <div className={styles.optionsR}>
+                            <h1 className={styles.singleOptionR}>Both A and B</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
