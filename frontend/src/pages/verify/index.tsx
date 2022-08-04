360ede31e9ee1d838d9594c026589ad5c8d37cc5
jsureka <bsse1115@iit.du.ac.bd>
2022-08-04 13:53:34 +0600

    verify page design done

--- frontend/src/pages/verify/index.tsx
+++ frontend/src/pages/verify/index.tsx
@@ -1,4 +1,9 @@
 import styles from 'styles/Verify.module.scss'
+import Image from 'next/image'
+import certificate from '../../../public/certificate.png'
+import badge_background from '../../../public/badge_background.png'
+import { auto } from '@popperjs/core'
+
 export default function Verify() {
   return (
     <div>
@@ -11,25 +16,13 @@ export default function Verify() {
             <label className="sr-only">Search</label>
             <div className=" w-full">
               <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-                <svg
-                  aria-hidden="true"
-                  className="h-5 w-5 text-gray-500 dark:text-gray-400"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
+                
               </div>
               <input
                 type="text"
                 id="simple-search"
                 className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 pl-10 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500  dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-                placeholder="Search"
+                placeholder="Enter your token ID"
                 data-ms-editor="true"
               ></input>
             </div>
@@ -55,7 +48,19 @@ export default function Verify() {
             </button>
           </form>
         </div>
-        <div className=' col-span-3'></div>
+        <div className=" col-span-3"></div>
+      </div>
+      <div className=" grid grid-cols-12">
+        <div className=" col-span-2"></div>
+          <div className={styles.image1}>
+            <Image src={certificate} width={700} height={500}></Image>
+          </div>
+
+          <div className={styles.image2}>
+            <h1>Your Name</h1>
+            <h4>TimeStamp</h4>
+          </div>
+        <div className=" col-span-2"></div>
       </div>
     </div>
   )
