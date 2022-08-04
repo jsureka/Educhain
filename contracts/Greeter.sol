e98f4750e60ce7557c396bb85467c2a023bc6ece
jsureka <bsse1115@iit.du.ac.bd>
2022-08-04 06:06:52 +0600

    Certificate issue and verification completed

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -2,33 +2,102 @@
 pragma solidity ^0.8.15;
 
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/draft-ERC721Votes.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract Greeter {
-    string private greeting;
-    address private _owner;
+contract Greeter is ERC721, EIP712, ERC721URIStorage {
+  string private greeting;
+  address private _owner;
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIdCounter;
 
-    modifier onlyOwner() {
-        require(msg.sender == _owner, "Not an owner");
-        _;
+  // struct Course {
+  //     uint128 course_id;
+  //     uint128 currentCheckpoints;
+  // }
+  struct Student {
+    address from;
+    uint256 timestamp;
+    string name;
+    uint128 course_id;
+    uint128 currentCheckpoints;
+  }
+
+  Student[] students;
+  modifier onlyOwner() {
+    require(msg.sender == _owner, "Not an owner");
+    _;
+  }
+
+  constructor() ERC721("EduChain", "EDC") EIP712("EduChain", "1") {
+    console.log("Deployed the Contract:");
+  }
+
+  function safeMint(address to, string memory uri) public  {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
     }
 
-    constructor(string memory _greeting) {
-        console.log("Deploying a Greeter with greeting:", _greeting);
-        _owner = msg.sender;
-        greeting = _greeting;
+      function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+
+        
+        super._burn(tokenId);
     }
 
-    function greet() public view returns (string memory) {
-        return greeting;
-    }
+  function putStake(uint128 course_id) public payable {
+    require(msg.value == 10000000000000, "Not enough ETH to enroll");
+    students.push(
+      Student(msg.sender, block.timestamp, "Mustahid", course_id, 0)
+    );
+  }
 
-    function setGreeting(string memory _greeting) public onlyOwner payable{
-        require(msg.value > 10000, "Not enough paid");
-        console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
-        greeting = _greeting;
-    }
+  function transferValue() external {
+    for (uint256 i; i < students.length; i++) {
+      if (students[i].from == msg.sender) {
+        require(students[i].currentCheckpoints <= 4, "Checkpoints Completed!");
+        uint128 sendValue = 10000000000000 / 4;
+        (bool sent, bytes memory data) = msg.sender.call{ value: sendValue }(
+          ""
+        );
 
-    function getOwner() public view returns(address) {
-        return _owner;
+        // require(owner.send(address(this).balance));
+
+        require(sent, "Failed to send Ether");
+        students[i].currentCheckpoints++;
+      }
+    }
+  }
+
+  function _afterTokenTransfer(
+    address from,
+    address to,
+    uint256 tokenId
+  ) internal override(ERC721) {
+    super._afterTokenTransfer(from, to, tokenId);
+  }
+
+  function _beforeTokenTransfer(
+    address from,
+    address to,
+    uint256 tokenId
+  ) internal override(ERC721) {
+    require(from == address(0), "Err: token is SOUL BOUND");
+    super._beforeTokenTransfer(from, to, tokenId);
+  }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
     }
 }
